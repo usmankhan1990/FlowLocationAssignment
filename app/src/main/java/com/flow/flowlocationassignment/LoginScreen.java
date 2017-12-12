06e78e05309fd6f774934d4282587eed40ad58bb
usmankhan1990 <m.usmankhan90@hotmail.com>
2017-12-12 22:17:48 +0400

    Login Screen
    Login Screen

--- app/src/main/java/com/flow/flowlocationassignment/LoginScreen.java
+++ app/src/main/java/com/flow/flowlocationassignment/LoginScreen.java
@@ -1,8 +1,10 @@
 package com.flow.flowlocationassignment;
 
+import android.app.Activity;
+
 /**
  * Created by UsmanKhan on 12/12/17.
  */
 
-public class LoginScreen {
+public class LoginScreen extends Activity {
 }
